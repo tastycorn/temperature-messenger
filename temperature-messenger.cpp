ea09d80a0b76475486af2713076ab79f2ab35ec0
tastycorn <ryan.peterson@ualberta.net>
2020-10-23 15:14:08 -0600

    Move curl into main

--- temperature-messenger.cpp
+++ temperature-messenger.cpp
@@ -23,7 +23,40 @@ int main()
     eventContent.append("\u00B0");
     eventContent.append("C.");
 
-    sendProwlMessage(apiKey,applicationName,eventContent,descriptionContent,messagePriority);
+    CURL *curl;
+    CURLcode result;
+    std::string theURL="https://api.prowlapp.com/publicapi/add?";
+    theURL.append("apikey=");
+    theURL.append(apiKey);
+    theURL.append("&application=");
+    theURL.append(applicationName);
+    theURL.append("&event=");
+    theURL.append(eventContent);
+    theURL.append("&description=");
+    theURL.append(descriptionContent);
+    theURL.append("&priority=");
+    theURL.append(messagePriority);
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl,CURLOPT_URL,theURL);
+        result = curl_easy_perform(curl);
+        if (result != CURLE_OK)
+        {
+            std::cerr << "Error during curl request: " << curl_easy_strerror(result) << std::endl;
+        }
+        curl_easy_cleanup(curl);
+    }
+    else
+    {
+        std::cerr << "Error handling curl." << std::endl;
+    }
+    curl_global_cleanup();
+
+    // sendProwlMessage(apiKey,applicationName,eventContent,descriptionContent,messagePriority);
 
     return 0;
 }
