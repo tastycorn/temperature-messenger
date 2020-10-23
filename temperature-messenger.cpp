7a383167ed0f379dae8b33da41db7e2a63c40ff1
tastycorn <ryan.peterson@ualberta.net>
2020-10-23 14:35:45 -0600

    Updated test to use rawTemp/1000

--- temperature-messenger.cpp
+++ temperature-messenger.cpp
@@ -10,10 +10,14 @@ int main()
     std::string descriptionContent = "Message sent to Prowl from Raspberry Pi temperature probe";
     std::string messagePriority = "2";
     
+    // Get the raw temperature value from the temperature probe and write it to rawTemp
     std::ifstream inf{"/sys/bus/w1/devices/28-00000a23e435/temperature"};
     int rawTemp;
     inf >> rawTemp;
-    std::cout << rawTemp;
+    
+    double degreesC = rawTemp/1000.0;
+
+    std::cout << degreesC;
 
     return 0;
 }
